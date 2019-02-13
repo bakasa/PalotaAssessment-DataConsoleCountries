34d629e3c3a654f385a97a87191c8a4f290f931b
Kholofelo Moyaba <kmmoyaba@gmail.com>
2019-02-13 08:49:13 +0200

    chore: add hints per question

--- PalotaInterviewCS/Program.cs
+++ PalotaInterviewCS/Program.cs
@@ -11,60 +11,113 @@ namespace PalotaInterviewCS
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
-        private static readonly string countriesEndpoint = "https://restcountries.eu/rest/v2/all";
+        private const string countriesEndpoint = "https://restcountries.eu/rest/v2/all";
 
         static void Main(string[] args)
         {
             Country[] countries = GetCountries(countriesEndpoint).GetAwaiter().GetResult();
-
-            //TODO use data operations and data structures to optimally find the correct value (N.B. be aware of null values)
-
-            Random rnd = new Random();
-            int southAfricanGiniPlace = rnd.Next(1, 10); // Use correct value
-
-            string lowestGiniCountry = "ExampleCountry"; // Use correct value
-
-            string countryWithMostRegions = "ExampleRegion"; // Use correct value
-            int amountOfTimezonesInRegion = rnd.Next(1, 10); // Use correct value
-
-            string mostPopularCurrency = "ExampleCurrency"; // Use correct value
-            int numCountriesUsedByCurrency = rnd.Next(1, 10); // Use correct value
-
-            string countryWithBorderingCountries = "ExampleCountry"; // Use correct value
-            int numberOfBorderingCountries = rnd.Next(1, 10); // Use correct value
-            int combinnedPopulation = rnd.Next(1000000, 10000000); // Use correct value
-
-            string mostPopularLanguage1 = "ExampleLanguage"; // Use correct value
-            string mostPopularLanguage2 = "ExampleLanguage"; // Use correct value
-            string mostPopularLanguage3 = "ExampleLanguage"; // Use correct value
-
-            string lowPopDensityName = "ExampleCountry"; // Use correct value
-            double lowPopDensity = rnd.NextDouble() * 100; // Use correct value
-            string highPopDensityName = "ExampleCountry"; // Use correct value
-            double highPopDensity = rnd.NextDouble() * 100; // Use correct value
-
-            string largestAreaSubregion = "ExampleSubRegion"; // Use correct value
-
-            string mostEqualRegionalBlock = "ExampleRegionalBlock"; // Use correct value
-            double lowestRegionalBlockGini = rnd.NextDouble() * 10; // Use correct value
 
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Palota Interview: Country Facts");
             Console.WriteLine();
             Console.ResetColor();
 
-            Console.WriteLine("1. South Africa's Gini coefficient is the {0} highest", GetOrdinal(southAfricanGiniPlace));
-            Console.WriteLine("2. {0} has the lowest Gini Coefficient", lowestGiniCountry);
-            Console.WriteLine("3. {0} is the region that spans most timezones at {1} timezones", countryWithMostRegions, amountOfTimezonesInRegion);
-            Console.WriteLine("4. {0} is the most popular currency and is used in {1} countries", mostPopularCurrency, numCountriesUsedByCurrency);
-            Console.WriteLine("5. The top three popular languages are {0}, {1} and {2}", mostPopularLanguage1, mostPopularLanguage2, mostPopularLanguage3);
-            Console.WriteLine("6. {0} and it's {1} bordering countries has the highest combined population of {2}", countryWithBorderingCountries, numberOfBorderingCountries, combinnedPopulation);
-            Console.WriteLine("7. {0} has the lowest population density of {1}", lowPopDensityName, lowPopDensity);
-            Console.WriteLine("8. {0} has the highest population density of {1}", highPopDensityName, highPopDensity);
-            Console.WriteLine("9. {0} is the subregion that covers the most area", largestAreaSubregion);
-            Console.WriteLine("10. {0} is the regional block with the lowest average Gini coefficient of {1}", mostEqualRegionalBlock,lowestRegionalBlockGini);
+            Random rnd = new Random(); // random to populate fake answer - you can remove this once you use real values
+
+            //TODO use data operations and data structures to optimally find the correct value (N.B. be aware of null values)
+
+            /*
+             * HINT: Sort the list in descending order to find South Africa's place in terms of gini coefficients
+             * `Country.Gini` is the relevant field to use here           
+             */
+            int southAfricanGiniPlace = rnd.Next(1, 10); // Use correct value
+            Console.WriteLine($"1. South Africa's Gini coefficient is the {GetOrdinal(southAfricanGiniPlace)} highest");
+
+            /*
+             * HINT: Sort the list in ascending order or just find the Country with the minimum gini coeficient          
+             * use `Country.Gini` for the ordering then return the relevant country's name `Country.Name`
+             */
+            string lowestGiniCountry = "ExampleCountry"; // Use correct value
+            Console.WriteLine($"2. {lowestGiniCountry} has the lowest Gini Coefficient");
+
+            /*
+             * HINT: Group by regions (`Country.Region`), then count the number of unique timezones that the countries in each region span
+             * Once you have done the grouping, find the group `Region` with the most timezones and return it's name and the number of unique timezones found          
+             */
+            string regionWithMostTimezones = "ExampleRegion"; // Use correct value
+            int amountOfTimezonesInRegion = rnd.Next(1, 10); // Use correct value
+            Console.WriteLine($"3. {regionWithMostTimezones} is the region that spans most timezones at {amountOfTimezonesInRegion} timezones");
+
+            /*
+             * HINT: Count occurances of each currency in all countries (check `Country.Currencies`)
+             * Find the name of the currency with most occurances and return it's name (`Currency.Name`) also return the number of occurances found for that currency          
+             */
+            string mostPopularCurrency = "ExampleCurrency"; // Use correct value
+            int numCountriesUsedByCurrency = rnd.Next(1, 10); // Use correct value
+            Console.WriteLine($"4. {mostPopularCurrency} is the most popular currency and is used in {numCountriesUsedByCurrency} countries");
+
+            /*
+             * HINT: Count the number of occurances of each language (`Country.Languages`) and sort then in descending occurances count (i.e. most populat first)
+             * Once done return the names of the top three languages (`Language.Name`)
+             */
+            string[] mostPopularLanguages = { "ExampleLanguage1", "ExampleLanguage2", "ExampleLanguage3" }; // Use correct values
+            Console.WriteLine($"5. The top three popular languages are {mostPopularLanguages[0]}, {mostPopularLanguages[1]} and {mostPopularLanguages[2]}");
+
+            /*
+             * HINT: Each country has an array of Bordering countries `Country.Borders`, The array has a list of alpha3 codes of each bordering country `Country.alpha3Code`
+             * Sum up the population of each country (`Country.Population`) along with all of its bordering countries's population. Sort this list according to the combined population descending
+             * Find the country with the highest combined (with bordering countries) population the return that country's name (`Country.Name`), the number of it's Bordering countries (`Country.Borders.length`) and the combined population
+             * Be wary of null values           
+             */
+            string countryWithBorderingCountries = "ExampleCountry"; // Use correct value
+            int numberOfBorderingCountries = rnd.Next(1, 10); // Use correct value
+            int combinedPopulation = rnd.Next(1000000, 10000000); // Use correct value
+            Console.WriteLine($"6. {countryWithBorderingCountries} and it's {numberOfBorderingCountries} bordering countries has the highest combined population of {combinedPopulation}");
+
+            /*
+             * HINT: Population density is calculated as (population size)/area, i.e. `Country.Population/Country.Area`
+             * Calculate the population density of each country and sort by that value to find the lowest density
+             * Return the name of that country (`Country.Name`) and its calculated density.
+             * Be wary of null values when doing calculations           
+             */
+            string lowPopDensityName = "ExampleCountry"; // Use correct value
+            double lowPopDensity = rnd.NextDouble() * 100; // Use correct value
+            Console.WriteLine($"7. {lowPopDensityName} has the lowest population density of {lowPopDensity}");
+
+            /*
+             * HINT: Population density is calculated as (population size)/area, i.e. `Country.Population/Country.Area`
+             * Calculate the population density of each country and sort by that value to find the highest density
+             * Return the name of that country (`Country.Name`) and its calculated density.
+             * Be wary of any null values when doing calculations. Consider reusing work from above related question           
+             */
+            string highPopDensityName = "ExampleCountry"; // Use correct value
+            double highPopDensity = rnd.NextDouble() * 100; // Use correct value
+            Console.WriteLine($"8. {highPopDensityName} has the highest population density of {highPopDensity}");
+
+            /*
+             * HINT: Group by subregion `Country.Subregion` and sum up the area (`Country.Area`) of all countries per subregion
+             * Sort the subregions by the combined area to find the maximum (or just find the maximum)
+             * Return the name of the subregion
+             * Be wary of any null values when summing up the area           
+             */
+            string largestAreaSubregion = "ExampleSubRegion"; // Use correct value
+            Console.WriteLine($"9. {largestAreaSubregion} is the subregion that covers the most area");
+
+            /*
+             * HINT: Group by regional blocks (`Country.RegionalBlocks`). For each regional block, average out the gini coefficient (`Country.Gini`) of all member countries
+             * Sort the regional blocks by the average country gini coefficient to find the lowest (or find the lowest without sorting)
+             * Return the name of the regional block (`RegionalBloc.Name`) along with the calculated average gini coefficient
+             */
+            string mostEqualRegionalBlock = "ExampleRegionalBlock"; // Use correct value
+            double lowestRegionalBlockGini = rnd.NextDouble() * 10; // Use correct value
+            Console.WriteLine($"10. {mostEqualRegionalBlock} is the regional block with the lowest average Gini coefficient of {lowestRegionalBlockGini}");
         }
 
+        /// <summary>
+        /// Gets the countries from a specified endpiny
+        /// </summary>
+        /// <returns>The countries.</returns>
+        /// <param name="path">Path endpoint for the API.</param>
         static async Task<Country[]> GetCountries(string path)
         {
             Country[] countries = null;
